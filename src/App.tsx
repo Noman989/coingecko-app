717c51d761223280d1749846dd84ab4ab8a5dfbb
noman980 <nauman@authornate.com>
2022-05-16 22:59:28 -0700

    Nav Done

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,17 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Nav } from './components/nav';
+import { Routes, Route } from 'react-router-dom';
+import { Home } from './pages/Home';
 
 function App() {
   return (
     <div>
       <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/compare" element={<div>Compare</div>} />
+      </Routes>
     </div>
   );
 }
--- src/components/nav/nav.tsx
+++ src/components/nav/nav.tsx
@@ -1,13 +1,56 @@
-import React from 'react';
+import React from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 
 export interface NavProps {
-    name?: string;
+  name?: string;
 }
 export const Nav: React.FC<NavProps> = () => {
+  const location = useLocation();
+  const [currentPath, setCurrentPath] = React.useState(location.pathname);
 
-    return (
-        <div>
-            <h1>Nav</h1>
+  React.useEffect(() => {
+    setCurrentPath(location.pathname);
+  }, [location]);
+
+  const nav = useNavigate();
+  const goto = (path: string) => {
+    nav(path);
+  };
+  return (
+    <div className="flex items-center justify-between px-12 bg-cyan-600 text-white h-[72px] w-full">
+      <div className="flex items-center gap-12">
+        <h1 className="text-3xl font-bold">Logo.</h1>
+        <div className="flex gap-6 font-bold">
+          <a
+            href="#"
+            onClick={(e) => {
+              e.preventDefault();
+              goto("/");
+            }}
+            className={`${currentPath === '/' && 'text-cyan-100'}`}
+            >
+            Home
+          </a>
+          <a
+            href="#"
+            onClick={(e) => {
+              e.preventDefault();
+              goto("/compare");
+            }}
+            className={`${currentPath === '/compare' && 'text-cyan-100'}`}
+          >
+            Compare
+          </a>
         </div>
-    )
-}
+      </div>
+      <div>
+        <input
+          type="text"
+          id="coinId-navbar"
+          className="block p-2 pl-10 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:text-sm focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          placeholder="CoinId..."
+        />
+      </div>
+    </div>
+  );
+};
