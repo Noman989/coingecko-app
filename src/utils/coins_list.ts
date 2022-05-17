2db5bae4fe028a3b3d36c81ce3af6af46c4cc504
noman980 <nauman@authornate.com>
2022-05-16 23:28:43 -0700

    added and updated util files

---
+++ src/utils/coins_list.ts
@@ -0,0 +1,29 @@
+export const CoinsList = {
+  new: {
+    id: "favor",
+    symbol: "favor",
+    name: "Favor",
+  },
+  compare_list: [
+    {
+      id: "bitcoin",
+      symbol: "btc",
+      name: "Bitcoin",
+    },
+    {
+      id: "ethereum",
+      symbol: "eth",
+      name: "Ethereum",
+    },
+    {
+      id: "solana",
+      symbol: "sol",
+      name: "Solana",
+    },
+    {
+      id: "cardano",
+      symbol: "ada",
+      name: "Cardano",
+    },
+  ],
+};
