a0a7c8f7a4d67a5cc077662726dfdf1dbccbb962
noman980 <nauman@authornate.com>
2022-05-17 05:48:24 -0700

    moved use effect from home to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { Nav } from './components/nav';
 import { Routes, Route } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { Compare } from './pages/Compare';
 import { CoinId } from './pages/CoinId';
+import { pushCoin } from "./components/table/table.slice";
+import { getGeneralData, getVolumeAndPercent } from "./api/coingecko";
+import { useDispatch } from "react-redux";
+import { CoinsList } from "./utils/coins_list";
 
 function App() {
+  const dispatch = useDispatch();
+
+  React.useEffect(() => {
+
+    (async () => {
+      let counter = 0;
+      const interval = setInterval(async () => {
+        const coin = await getGeneralData(CoinsList.compare_list[counter].id);
+        dispatch(pushCoin(coin));
+        counter++;
+        if (!CoinsList.compare_list[counter]) clearInterval(interval);
+      }, 300);
+
+      await getVolumeAndPercent("bitcoin");
+    })();
+  }, []);
+
   return (
     <div>
       <Nav />
--- src/pages/Home/home.tsx
+++ src/pages/Home/home.tsx
@@ -1,31 +1,9 @@
 import React from "react";
 import { Table } from "../../components/table";
-import { setCoinsList, pushCoin, selectTable } from "../../components/table/table.slice";
-import { getGeneralData, getMarketChart, getVolumeAndPercent } from "../../api/coingecko";
-import { useDispatch, useSelector } from "react-redux";
-import { CoinsList } from "../../utils/coins_list";
-import { Chart } from "../../components/chart/chart";
 export interface HomeProps {
   name?: string;
 }
 export const Home: React.FC<HomeProps> = () => {
-  const dispatch = useDispatch();
-  const coins = useSelector(selectTable);
-
-  React.useEffect(() => {
-    (async () => {
-      let counter = 0;
-      const interval = setInterval(async () => {
-        const coin = await getGeneralData(CoinsList.compare_list[counter].id);
-        dispatch(pushCoin(coin));
-        counter++;
-        if (!CoinsList.compare_list[counter]) clearInterval(interval);
-      }, 300);
-
-      await getVolumeAndPercent("bitcoin");
-    })();
-  }, []);
-
   return (
     <div className="flex justify-center mt-12">
       <Table />
