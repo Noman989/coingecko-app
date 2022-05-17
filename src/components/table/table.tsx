f9a4bd1a5e142eb820a864af9e61dcff04ebe50e
noman980 <nauman@authornate.com>
2022-05-17 00:50:14 -0700

    added table row component

--- src/components/table/table.tsx
+++ src/components/table/table.tsx
@@ -1,6 +1,52 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { selectTable } from "./table.slice";
+
+interface TableRowProps {
+  name: string;
+  thumbImageURI: string;
+  symbol: string;
+  _24percent: string;
+  _24hVolume: string;
+}
+const TableRow: React.FC<TableRowProps> = ({
+  name,
+  _24hVolume,
+  _24percent,
+  symbol,
+  thumbImageURI,
+}: TableRowProps) => {
+  return (
+    <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+      <th
+        scope="row"
+        className="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap flex items-center gap-2"
+      >
+        <span><img src={thumbImageURI} alt={symbol}></img></span>
+        <span>{name}</span>
+      </th>
+      <td className="px-6 py-4">{symbol}</td>
+      <td className="px-6 py-4">{_24percent}</td>
+      <td className="px-6 py-4">{_24hVolume}</td>
+      <td className="px-6 py-4 text-right">
+        <a
+          href="#"
+          className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+        >
+          More
+        </a>
+      </td>
+    </tr>
+  );
+};
 
 export const Table = () => {
+  const coins = useSelector(selectTable);
+
+  React.useEffect(() => {
+    console.log(coins);
+  }, [coins]);
+
   return (
     <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
       <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
@@ -13,7 +59,7 @@ export const Table = () => {
               Symbol
             </th>
             <th scope="col" className="px-6 py-3">
-              7d
+              1d
             </th>
             <th scope="col" className="px-6 py-3">
               24h Volume
@@ -24,25 +70,16 @@ export const Table = () => {
           </tr>
         </thead>
         <tbody>
-          <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-            <th
-              scope="row"
-              className="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap"
-            >
-              Bitcoin
-            </th>
-            <td className="px-6 py-4">BTC</td>
-            <td className="px-6 py-4">22%</td>
-            <td className="px-6 py-4">12334123124</td>
-            <td className="px-6 py-4 text-right">
-              <a
-                href="#"
-                className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-              >
-                More
-              </a>
-            </td>
-          </tr>
+          {coins.map((coin) => (
+            <TableRow
+              key={coin.id}
+              name={coin.name}
+              thumbImageURI={coin.image.thumb}
+              symbol={coin.symbol}
+              _24percent={coin._24Percent}
+              _24hVolume={coin._24hVolume}
+            ></TableRow>
+          ))}
         </tbody>
       </table>
     </div>
