9dde39aef78959b1fc0e7006be29ef2097b62167
noman980 <nauman@authornate.com>
2022-05-17 02:52:31 -0700

    API call methods

--- src/api/coingecko.ts
+++ src/api/coingecko.ts
@@ -1,4 +1,4 @@
-import { Coin } from '../components/table/table.slice'
+import { Coin } from "../components/table/table.slice";
 export const getGeneralData = async (id: string) => {
   const res = await fetch(
     `https://api.coingecko.com/api/v3/coins/${id}?localization=false&tickers=false&market_data=false&community_data=false&developer_data=false&sparkline=true`,
@@ -7,6 +7,9 @@ export const getGeneralData = async (id: string) => {
 
   const data = await res.json();
   //   console.log(data);
+  const vp = await getVolumeAndPercent(id);
+
+  const chartData = await getChartData(id);
 
   const coin: Coin = {
     id: id,
@@ -16,9 +19,11 @@ export const getGeneralData = async (id: string) => {
       small: data.image.small,
       thumb: data.image.thumb,
     },
-    _24Percent: "22",
-    _24hVolume: "2222222",
+    _24Percent: `${vp.percent.percent}`,
+    gain: vp.percent.gain,
+    _24hVolume: `${vp.volume}`,
     symbol: data.symbol,
+    chartData
   };
 
   return coin;
@@ -32,4 +37,55 @@ export const getMarketChart = async (id: string) => {
 
   const data = await res.json();
   console.log(data);
+};
+
+export const calcPercent = (init: number, final: number) => {
+  if (init < final) {
+    return {
+      gain: true,
+      percent: (final - init) / final,
+    };
+  } else if (init > final) {
+    return {
+      gain: false,
+      percent: (init - final) / final,
+    };
+  } else {
+    return {
+      gain: false,
+      percent: 0,
+    };
+  }
+};
+
+export const calcVolume = (init: number, final: number) => {
+  return (init + final) / 2;
+};
+
+export const getVolumeAndPercent = async (id: string) => {
+  const res = await fetch(
+    `https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=usd&days=1&interval=daily`,
+    { method: "GET" }
+  );
+
+  const data = await res.json();
+  const percent = calcPercent(data.prices[0][1], data.prices[1][1]);
+
+  const volume = calcVolume(data.total_volumes[0][1], data.total_volumes[1][1]);
+
+  return { percent, volume };
+};
+
+export const getChartData = async (id: string) => {
+  const res = await fetch(
+    `https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=usd&days=7&interval=daily`,
+    { method: "GET" }
+  );
+
+  const data = await res.json();
+
+  const returnData = data.prices.map((p: number[]) => p[1]);
+  returnData.shift();
+  console.log(returnData);
+  return returnData;
 };
